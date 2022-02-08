e14f4b250206427383b58cc1512ed7097faaa976
minholeelog <91aiden.lee@gmail.com>
2022-02-09 00:21:51 +0900

    ClickToEdit 컴포넌트 완성

--- custom-component/src/components/ClickToEdit/ClickToEdit.js
+++ custom-component/src/components/ClickToEdit/ClickToEdit.js
@@ -4,9 +4,18 @@ import styles from './ClickToEdit.module.css'
 function ClickToEdit() {
   const [form, setForm] = useState({
     username: '이민호',
-    age: '32',
+    age: '30',
   })
-  const [isActive, setIsAcitve] = useState(false)
+
+  const [submitForm, setSubmitForm] = useState({
+    username: '이민호',
+    age: 30,
+  })
+
+  const [isActive, setIsAcitve] = useState({
+    username: false,
+    age: false,
+  })
 
   const onChange = (event) => {
     const {
@@ -30,8 +39,160 @@ function ClickToEdit() {
     }
   }
 
-  const onSubmit = (event) => {
-    event.preventDefault()
+  const onBlur = (event) => {
+    const {
+      target: { name, value },
+    } = event
+    switch (name) {
+      case 'username':
+        setIsAcitve({
+          ...isActive,
+          username: false,
+        })
+        setForm({
+          ...form,
+          username: value,
+        })
+        setSubmitForm({
+          ...form,
+          username: value,
+        })
+        break
+      case 'age':
+        setIsAcitve({
+          ...isActive,
+          age: false,
+        })
+        setForm({
+          ...form,
+          age: value,
+        })
+        setSubmitForm({
+          ...form,
+          age: value,
+        })
+        break
+      default:
+        return
+    }
+    setIsAcitve({
+      username: false,
+      age: false,
+    })
+  }
+
+  const onDoubleClick = (event) => {
+    const {
+      target: { name, id },
+    } = event
+    if (typeof name !== 'undefined') {
+      switch (name) {
+        case 'username':
+          setIsAcitve({
+            ...isActive,
+            username: !isActive.username,
+          })
+          break
+        case 'age':
+          setIsAcitve({
+            ...isActive,
+            age: !isActive.age,
+          })
+          break
+        default:
+          return
+      }
+    }
+    switch (id) {
+      case 'username':
+        setIsAcitve({
+          ...isActive,
+          username: !isActive.username,
+        })
+        break
+      case 'age':
+        setIsAcitve({
+          ...isActive,
+          age: !isActive.age,
+        })
+        break
+      default:
+        return
+    }
+  }
+
+  const IsActiveUsername = () => {
+    return (
+      <>
+        <div
+          className={`${styles.inputBlock} ${
+            !isActive.username ? styles.active : ''
+          }`}
+        >
+          <span className={styles.inputLabel}>이름</span>
+          {isActive.username ? (
+            <>
+              <input
+                name="username"
+                value={form.username}
+                onChange={onChange}
+                onDoubleClick={onDoubleClick}
+                onBlur={onBlur}
+                autoFocus
+              />
+            </>
+          ) : (
+            <>
+              <div
+                id="username"
+                value={form.username}
+                onChange={onChange}
+                onDoubleClick={onDoubleClick}
+              >
+                {form.username}
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    )
+  }
+  const IsActiveAge = () => {
+    return (
+      <>
+        <div
+          className={`${styles.inputBlock} ${
+            !isActive.username ? styles.active : ''
+          }`}
+        >
+          <span className={styles.inputLabel}>나이</span>
+          {isActive.age ? (
+            <>
+              <input
+                name="age"
+                value={form.age}
+                onChange={onChange}
+                onDoubleClick={onDoubleClick}
+                onBlur={onBlur}
+                autoFocus
+              />
+            </>
+          ) : (
+            <>
+              <div
+                id="age"
+                value={form.age}
+                onChange={onChange}
+                onDoubleClick={onDoubleClick}
+                className={styles.disabled}
+              >
+                {form.age}
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    )
   }
 
   return (
@@ -39,48 +200,11 @@ function ClickToEdit() {
       <h2 className={styles.title}>ClickToEdit</h2>
       <div className={styles.clickToEditContainer}>
         <form>
-          {isActive ? (
-            <>
-              <div className={`${styles.inputBlock} ${styles.active}`}>
-                <span>이름</span>
-                <input
-                  className={styles.active}
-                  name="username"
-                  placeholder="이름"
-                  value={form.username}
-                  onChange={onChange}
-                />
-              </div>
-              <div className={`${styles.inputBlock} ${styles.active}`}>
-                <span>나이</span>
-                <input
-                  className={styles.active}
-                  name="age"
-                  placeholder="나이"
-                  value={form.age}
-                  onChange={onChange}
-                />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className={styles.inputBlock}>
-                <span>이름</span>
-                <div className={styles.disabled} onChange={onChange}>
-                  {form.username}
-                </div>
-              </div>
-              <div className={styles.inputBlock}>
-                <span>나이</span>
-                <div className={styles.disabled} onChange={onChange}>
-                  {form.age}
-                </div>
-              </div>
-            </>
-          )}
+          <IsActiveUsername />
+          <IsActiveAge />
         </form>
         <div>
-          이름 {form.username} 나이 {form.age}
+          이름 {submitForm.username} 나이 {submitForm.age}
         </div>
       </div>
     </>
