07559748035755f2146f8af9d0ae9f92104212c3
minholeelog <91aiden.lee@gmail.com>
2022-02-09 13:13:04 +0900

    Modal 컴포넌트 완성

--- custom-component/src/components/Modal/Modal.js
+++ custom-component/src/components/Modal/Modal.js
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+import styles from './Modal.module.css'
+
 function Modal() {
-  return <div>Modal</div>
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onToggleModal = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <>
+      {isOpen ? (
+        <div className={styles.modalOpenContainer}>
+          <h2 className={styles.title}>Modal</h2>
+          <div className={styles.modalBox}>
+            <button onClick={onToggleModal} className={styles.modalCloseButton}>
+              X
+            </button>
+            <span className={styles.modalContent}>Hello, CodeStates</span>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h2 className={styles.title}>Modal</h2>
+          <div className={styles.modalContainer}>
+            <div onClick={onToggleModal} className={styles.modalButton}>
+              Open Modal
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Modal
