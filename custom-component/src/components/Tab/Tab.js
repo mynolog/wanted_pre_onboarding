3696c77da167eee821f338c11014e9ba7dd8ad59
minholeelog <91aiden.lee@gmail.com>
2022-02-10 11:08:52 +0900

    Tab 컴포넌트 완성

--- custom-component/src/components/Tab/Tab.js
+++ custom-component/src/components/Tab/Tab.js
@@ -1,5 +1,39 @@
+import { useState } from 'react'
+import { tabContents } from './tabContents'
+import styles from './Tab.module.css'
+
 function Tab() {
-  return <div>Tab</div>
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const onChangeTab = (index) => {
+    setCurrentIndex(index)
+  }
+
+  return (
+    <>
+      <h2 className={styles.title}>Tab</h2>
+      <div className={styles.tabContainer}>
+        <div className={styles.tabNavigator}>
+          <ul className={styles.tabNavList}>
+            {tabContents.map((tabContent, index) => (
+              <li
+                className={`${styles.tabNavItem} ${
+                  index === currentIndex ? styles.active : ''
+                }`}
+                onClick={() => onChangeTab(index)}
+                key={tabContent.id}
+              >
+                {tabContent.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.tabContentBox}>
+          <div>{tabContents[currentIndex].content}</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Tab
---
+++ custom-component/src/components/Tab/tabContents.js
@@ -0,0 +1,17 @@
+export const tabContents = [
+  {
+    id: 1,
+    title: 'Tab 1',
+    content: 'Tab Menu ONE',
+  },
+  {
+    id: 2,
+    title: 'Tab 2',
+    content: 'Tab Menu TWO',
+  },
+  {
+    id: 3,
+    title: 'Tab 3',
+    content: 'Tab Menu THREE',
+  },
+]
